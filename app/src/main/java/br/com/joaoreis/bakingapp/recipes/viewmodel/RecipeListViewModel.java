10fe82cc386b2b21af1049f0e053494009134068
Joao Ubaldo Xavier Reis <joao.reis21@gmail.com>
2019-06-30 15:13:01 -0300

    Adding viewmodel

---
+++ app/src/main/java/br/com/joaoreis/bakingapp/recipes/viewmodel/RecipeListViewModel.java
@@ -0,0 +1,7 @@
+package br.com.joaoreis.bakingapp.recipes.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class RecipeListViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

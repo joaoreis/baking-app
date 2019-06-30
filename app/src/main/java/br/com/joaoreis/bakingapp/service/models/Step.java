2489b7425f5a1d558d8dac269093825857408e0b
Joao Ubaldo Xavier Reis <joao.reis21@gmail.com>
2019-06-30 15:12:07 -0300

    Adding models

---
+++ app/src/main/java/br/com/joaoreis/bakingapp/service/models/Step.java
@@ -0,0 +1,98 @@
+
+package br.com.joaoreis.bakingapp.service.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.squareup.moshi.Json;
+
+public class Step implements Parcelable {
+
+    @Json(name = "id")
+    private int id;
+
+    @Json(name = "shortDescription")
+    private String shortDescription;
+
+    @Json(name = "description")
+    private String description;
+
+    @Json(name = "videoURL")
+    private String videoURL;
+
+    @Json(name = "thumbnailURL")
+    private String thumbnailURL;
+
+    public static final Creator<Step> CREATOR = new Creator<Step>() {
+
+        public Step createFromParcel(Parcel in) {
+            return new Step(in);
+        }
+
+        public Step[] newArray(int size) {
+            return (new Step[size]);
+        }
+
+    };
+
+    private Step(Parcel in) {
+        this.id = in.readInt();
+        this.shortDescription = in.readString();
+        this.description = in.readString();
+        this.videoURL = in.readString();
+        this.thumbnailURL = in.readString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public void setVideoURL(String videoURL) {
+        this.videoURL = videoURL;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+
+    public void setThumbnailURL(String thumbnailURL) {
+        this.thumbnailURL = thumbnailURL;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(id);
+        dest.writeValue(shortDescription);
+        dest.writeValue(description);
+        dest.writeValue(videoURL);
+        dest.writeValue(thumbnailURL);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+}
